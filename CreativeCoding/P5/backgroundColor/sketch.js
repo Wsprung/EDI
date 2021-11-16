fc6930427d6e2af5f70d10e1327445d27f8f9709
Wsprung <whitneykin@gmail.com>
2021-11-16 16:58:53 -0500

    Update sketch.js

--- CreativeCoding/P5/backgroundColor/sketch.js
+++ CreativeCoding/P5/backgroundColor/sketch.js
@@ -1,7 +1,7 @@
 function setup() {
   createCanvas(windowWidth, windowHeight); /*This creates the window
   width and height to be the size of my browser window*/
-  background(175, 0, 46); //background(R, G, B)
+  background(10, 46, 46); //background(R, G, B)
 
 }
 
