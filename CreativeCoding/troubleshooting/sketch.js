a3ca0b2a4baf7d35e7b993e560b30c0498b79483
Wsprung <whitneykin@gmail.com>
2021-11-30 22:28:37 -0500

    Creates 2x2 of gif, not fully working yet

--- CreativeCoding/troubleshooting/sketch.js
+++ CreativeCoding/troubleshooting/sketch.js
@@ -1,8 +1,13 @@
 let img; //declaring image
 
 //loads image to variable to later display on screen
+let brainGifs = [];
+
 function preload() {
-  img = loadImage('assets/BrainFrames/BFv4/BFv4-01.png');
+  for(var i = 0; i < 4; i++) {
+    brainGifs[i] = createImg('assets/BrainFrames/BrainGifv2copy.gif');
+    //works once then stops:( FIX
+  }
 }
 
  function setup() {
@@ -18,20 +23,19 @@ function preload() {
 function draw() {
   //naturally, draw function runs in a loop
   background(220);
-
+  var j = 0;
 
 //iterates in y-direction to create rows in the y-direction
   for(var y = 0; y < 600; y = y + 300) {
 
  //iterates in x-direction to create row of squares
   for(var i = 0; i < 600; i = i + 300) {
-    //2x2 grid of load
-    //image(imgVar, x, y);
-    image(img, i, y);
+    //2x2 grid of image
+    brainGifs[j].position(i, y);
+    j++;
   }
 }
 
   translate(60, 60); //moves the origin point of the square
 
-  noLoop();
 }
