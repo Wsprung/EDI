953fc1ee74f8ef4339e153f072b46f77b0caaca3
Wsprung <whitneykin@gmail.com>
2021-12-03 16:34:19 -0500

    Changes website background and face rising gif

--- CreativeCoding/ResizingScreen/sketch.js
+++ CreativeCoding/ResizingScreen/sketch.js
@@ -1,7 +1,7 @@
 let gif;
 
 function preload() {
-  gif = loadImage('assets/gifTest.gif');
+  gif = loadImage('assets/FaceRisingNoLoop250.gif');
 }
 
 function setup() {
@@ -11,29 +11,38 @@ function setup() {
   //upper left corner = (0,0)
   //as x moves to the right it is increasing, as y moves down it is increasing
   //bottom right corner is (720, 720)
-  fill("red"); //filling the square with red
-  strokeWeight(1); //stroke weight of the square
+  //fill("red"); //filling the square with red
+  //strokeWeight(1); //stroke weight of the square
+  background(0,0,0);
 }
 
 function draw() {
+  background(0,0,0);
   //naturally, draw function runs in a loop
 
 //variable for the number of squares in the grid
-  var numSquares = 6;
+  //gif size not adjusting because createCanvas is recreating canvas with number of
+  //gifs based off new windowWith/300 -> will not resize gif
+  var numSquares = windowWidth/gif.width;
 
 //variable for the side length of each square
   var sideLen = windowWidth/numSquares;
-
 //iterates in y-direction to create rows in the y-direction
+ //var placeGif = true;
   for(var y = 0; y < windowHeight; y = y + sideLen) {
 
  //iterates in x-direction to create row of squares
   for(var x = 0; x < windowWidth; x = x + sideLen) {
-   image(gif, x, y);
-
+   // if(placeGif) { image(gif, x, y); }
+   // placeGif = !placeGif;
+  image(gif, x, y);
   }
 }
 }
 function windowResized() {
+  //gif.resize(0, gif.width*(.5));
   resizeCanvas(windowWidth, windowHeight);
 }
+//beginShape() -> look into to shrink gif
+//arc
+//translate
