c9219bdcb5089152634e4b6997e871d9976b63d7
Wsprung <whitneykin@gmail.com>
2022-01-05 22:22:38 -0500

    Adds randomRepeat, incrementation, and radial examples

--- CreativeCoding/MachDraw/incrementation/sketch.js
+++ CreativeCoding/MachDraw/incrementation/sketch.js
@@ -7,6 +7,8 @@ this will save an SVG file in your download folder
 
 var pW = 11; //width of the paper (axidraw)
 var pH = 8.5; //height of the paper (axidraw)
+var x = 0;
+var y = 0;
 
 function setup() {
   createCanvas(pW * 72, pH * 72, SVG); // Create SVG Canvas
@@ -14,21 +16,26 @@ function setup() {
   stroke(255, 0, 0); // red is good for laser
   noFill(); // better not to have a fill for laser
   background("blue");
+
+  translate(width/2, height/2);
+  //changing orientation of rectangles
+  rectMode(CENTER);
+
+  rect(0, 0, 100, 100);
+  //do this
+  do {
+    //rect(x sp, y sp, w of rect, h of rect)
+    rect(0, 0, 1 + x, 1 + y);
+    //incrementing the size of the rectangle each time the loop runs
+    x = x + random(0, 10);
+    y = y + random(0, 10);
+  }
+  //while this condition
+  while(x < 600);
+
 }
 
 function draw() {
-  //centering the design
-  translate((pW * 72 - 600)/2, (pH * 72 - 600)/2)
-
-  //line in the y dir'n
-  for (y = 0; y < 600; y = y + 100) {
-    //line in the x dir'n
-    for (x = 0; x < 600; x = x + 100) {
-      //random(min, max)
-      line(x, y, x + random(60, 100), y + random(60, 100));
-    }
-  }
-
   // save("mySVG.svg"); // give file name
   // print("saved svg");
   noLoop(); // we just want to export once
--- CreativeCoding/MachDraw/radial/sketch.js
+++ CreativeCoding/MachDraw/radial/sketch.js
@@ -5,27 +5,24 @@ see -https://github.com/zenozeng/p5.js-svg
 this will save an SVG file in your download folder
 */
 
-var pW = 11; //width of the paper (axidraw)
-var pH = 8.5; //height of the paper (axidraw)
+var pW = 11; //paper width
+var pH = 8.5; //paper height
 
 function setup() {
   createCanvas(pW * 72, pH * 72, SVG); // Create SVG Canvas
   strokeWeight(1); // do 0.1 for laser
   stroke(255, 0, 0); // red is good for laser
   noFill(); // better not to have a fill for laser
-  background("blue");
+  // background("limegreen");
 }
 
 function draw() {
-  //centering the design
-  translate((pW * 72 - 600)/2, (pH * 72 - 600)/2)
-
-  //line in the y dir'n
-  for (y = 0; y < 600; y = y + 100) {
-    //line in the x dir'n
-    for (x = 0; x < 600; x = x + 100) {
-      //random(min, max)
-      line(x, y, x + random(60, 100), y + random(60, 100));
+  //centering
+  translate(width/2, height/2);
+  for(var x = -300; x < 300; x = x + 40) {
+    for(var y = -300; y < 300; y = y + 40) {
+      //line(start x, start y, px, px)
+      line(0, 0, x, y)
     }
   }
 
