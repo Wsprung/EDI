38242cd05a69c9c6d0da1e3508c31c64f946cc24
Wsprung <whitneykin@gmail.com>
2022-01-05 21:24:33 -0500

    Fixes bugs

--- CreativeCoding/MachDraw/randomRepeat/sketch.js
+++ CreativeCoding/MachDraw/randomRepeat/sketch.js
@@ -5,11 +5,15 @@ see -https://github.com/zenozeng/p5.js-svg
 this will save an SVG file in your download folder
 */
 
+var pW = 11; //width of the paper (axidraw)
+var pH = 8.5; //height of the paper (axidraw)
+
 function setup() {
-  createCanvas(200, 200, SVG); // Create SVG Canvas
+  createCanvas(pW * 72, pH * 72, SVG); // Create SVG Canvas
   strokeWeight(1); // do 0.1 for laser
   stroke(255, 0, 0); // red is good for laser
   noFill(); // better not to have a fill for laser
+  background(blue);
 }
 
 function draw() {
